c33cf2629d7e024c081273532bf7ed233cb57cff
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2020-08-21 14:57:32 +0300

    Task 5. Version 4.  - Add edit with canceling;  - Add deletion using Delete completed button;  - change findBy(text) to findBy(exactText).

--- src/test/java/com/taotas/todomvctests/PocTests.java
+++ src/test/java/com/taotas/todomvctests/PocTests.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
 import static com.codeborne.selenide.Condition.cssClass;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PocTests {
@@ -19,14 +19,25 @@ public class PocTests {
         // Create
         $("#new-todo").append("a").pressEnter();
         $("#new-todo").append("b").pressEnter();
-        $$("#todo-list>li").shouldHave(exactTexts("a", "b"));
+        $("#new-todo").append("c").pressEnter();
+        $$("#todo-list>li").shouldHave(exactTexts("a", "b", "c"));
 
         // Edit
-        $$("#todo-list>li").findBy(text("b")).doubleClick();
+        $$("#todo-list>li").findBy(exactText("a")).doubleClick();
         $$("#todo-list>li").find(cssClass("editing")).find(".edit").append("_edited").pressEnter();
 
-        // Delete
-        $$("#todo-list>li").findBy(text("b_edited")).hover().find(".destroy").click();
-        $$("#todo-list>li").shouldHave(exactTexts("a"));
+        // Delete by 'Clear completed' button
+        $$("#todo-list>li").findBy(exactText("a_edited")).find(".toggle").click();
+        $("#clear-completed").click();
+        $$("#todo-list>li").shouldHave(exactTexts("b", "c"));
+
+        // Edit with canceling
+        $$("#todo-list>li").findBy(exactText("c")).doubleClick();
+        $$("#todo-list>li").find(cssClass("editing")).find(".edit").append("_shouldn't be saved")
+                .pressEscape();
+
+        // Delete by 'x' button
+        $$("#todo-list>li").findBy(exactText("c")).hover().find(".destroy").click();
+        $$("#todo-list>li").shouldHave(exactTexts("b"));
     }
 }
