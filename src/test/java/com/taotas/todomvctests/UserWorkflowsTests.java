c31e07c227215170f236f42e116c7b98001190e1
Vitalii Ismagilov <ismagilov.vel@gmail.com>
2020-09-08 14:11:31 +0300

    Task 6. DRY. Final version (after video review)

--- src/test/java/com/taotas/todomvctests/UserWorkflowsTests.java
+++ src/test/java/com/taotas/todomvctests/UserWorkflowsTests.java
@@ -1,34 +1,43 @@
 package com.taotas.todomvctests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
 import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.jsReturnsValue;
 
 public class UserWorkflowsTests {
 
     @Test
     public void commonTasksManagement() {
         open("http://todomvc4tasj.herokuapp.com/");
-        Wait().until(ExpectedConditions
-                .jsReturnsValue("return $._data($('#new-todo').get(0), 'events').hasOwnProperty('keyup')"));
+        Wait().until(jsReturnsValue(
+                "return $._data($('#clear-completed').get(0), 'events')" +
+                        ".hasOwnProperty('click')"));
 
+        // Create
         add("a", "b", "c");
         todosShouldBe("a", "b", "c");
 
+        // Edit
         startEdit("a", " edited").pressEnter();
+
+        // Complete
         todo("a edited").find(".toggle").click();
 
+        // Clear Completed
         $("#clear-completed").click();
         todosShouldBe("b", "c");
 
+        // Cancel editing
         startEdit("c", "cancel editing").pressEscape();
 
+        // Delete
         todo("c").hover().find(".destroy").click();
         todosShouldBe("b");
     }
@@ -40,9 +49,9 @@ public class UserWorkflowsTests {
         }
     }
 
-    private SelenideElement startEdit(String text, String appendText) {
+    private SelenideElement startEdit(String text, String textToAdd) {
         todo(text).doubleClick();
-        return todos().find(cssClass("editing")).find(".edit").append(appendText);
+        return todoBy(cssClass("editing")).find(".edit").append(textToAdd);
     }
 
     private ElementsCollection todos() {
@@ -54,6 +63,10 @@ public class UserWorkflowsTests {
     }
 
     private SelenideElement todo(String text) {
-        return todos().findBy(exactText(text));
+        return todoBy(exactText(text));
+    }
+
+    private SelenideElement todoBy(Condition condition) {
+        return todos().findBy(condition);
     }
 }
